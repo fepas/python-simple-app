c98f3b2c325ad39f64a94630cf52edcc3019b7c8
erteduarda <erteduarda@gmail.com>
2021-03-05 22:36:38 -0300

    Entrega tarefa de python e flask

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask
 from flask import request
 
@@ -9,28 +10,63 @@ def post_user(user):
     
     if user not in users:
         users.append(user)
-        return f"o usuário {user} foi adicionado."
+        return f"o usuário {user['name']} foi adicionado."
     else:
-        return f"o usuário {user} já existe."
+        return f"o usuário {user['name']} já existe."
     
-def get_user(user):
-    if user in users:
-        return user
-    else:
-        return 'erro, usuario nao encontrado'
+def get_user(user_name):
+    for u in users:
+        if u['name'] == user_name:
+            u["sign"] = get_sign(u)
+            return u
+
+def get_sign(user):
+    day = int(user['birth_day'])
+    month = int(user['birth_month'])
+
+    print(day)
+    print(month)
+
+    if (month == 3 and day >= 21) or (month == 4 and day <= 19):
+        return 'Aries'
+    elif (month == 4 and day >= 20) or (month == 5 and day <= 20):
+        return 'Touro'
+    elif (month == 5 and day >= 21) or (month == 6 and day <= 20):
+        return 'Gêmeos'
+    elif (month == 6 and day >= 21) or (month == 7 and day <= 22):
+        return 'Câncer'
+    elif (month == 7 and day >= 23) or (month == 8 and day <= 22):
+        return 'Leão'
+    elif (month == 8 and day >= 23) or (month == 9 and day <= 22):
+        return 'Virgem'
+    elif (month == 9 and day >= 23) or (month == 10 and day <= 22):
+        return 'Libra'
+    elif (month == 10 and day >= 23) or (month == 11 and day <= 21):
+        return 'Escorpião'
+    elif (month == 11 and day >= 22) or (month == 12 and day <= 21):
+        return 'Sagitário'
+    elif (month == 12 and day >= 22) or (month == 1 and day <= 19):
+        return 'Capricórnio'
+    elif (month == 1 and day >= 20) or (month == 2 and day <= 18):
+        return 'Aquário'
+    elif (month == 2 and day >= 19) or (month == 3 and day <= 20):
+        return 'Peixes'     
 
 #################################
 
-@app.route('/')
-def hello():
-    return "Hello World!"
-
-@app.route('/user/<username>', methods=['GET', 'POST'])
-def user(username):
+@app.route('/register', methods=['POST'])
+def register():
     if request.method == 'POST':
-        return post_user(username)
-    else:
-        return get_user(username)
+        json = request.get_json()
+        return post_user(json)
+
+@app.route('/users')
+def list_users():
+        return json.dumps(users)
+
+@app.route('/sign/<name>')
+def sign(name):
+        return get_user(name)
 
 @app.route('/jsonrequest', methods=['GET', 'POST'])
 def jsonrequest():
